e7586c1b86a23befaa498cc0a9db8ec2c8997429
ASR-01 <gamingasr002@gmail.com>
2024-07-13 10:43:22 +0530

    edit validation_01

--- server/src/middlewares/user.middleware.ts
+++ server/src/middlewares/user.middleware.ts
@@ -1,21 +1,23 @@
-import { NextFunction, Request, Response } from 'express';
-import { ZodSchema } from 'zod';
-
+import { NextFunction, Request, Response } from "express";
+import { ZodSchema } from "zod";
 
 export const validateSchema =
   (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
-   
     const { success, error } = schema.safeParse(req.body);
 
-   1
+    1;
     if (!success) {
       return res.status(401).json({
         status: false,
         message: error.errors
-          .map((t) => `${t.path[0] ?? ''}: ${t.message}`)
-          .join(', '),
+          .map((err) => {
+            return `${err.path[0] ?? ""}: ${err.message}`;
+          })
+          .join(","),
       });
     }
 
     next();
   };
+
+
