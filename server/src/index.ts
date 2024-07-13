391bca2612fe2e2f386f078308b1bcf05531966e
ASR-01 <gamingasr002@gmail.com>
2024-07-13 09:54:04 +0530

    added prisma

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import Express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import userRoute from "./routes/index.routes";
+dotenv.config();
+
+const app = Express();
+
+app.use(Express.json());
+
+
+app.use("/api", userRoute);
+
+app.get("*", (req: Request, res: Response) =>
+  res.send("I  Richest man in the World!")
+);
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
