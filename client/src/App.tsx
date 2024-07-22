441cd3f624acc665a2841e185d0fdcaff45a15f0
ASR-01 <gamingasr002@gmail.com>
2024-07-22 12:19:14 +0530

    profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './App.css'
 // import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
+import Profile from './components/Profile/Profile'
 // import Signup from './Pages/Signup/Signup'
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Signup /> */}
+      <Profile/>
     </>
   )
 }
